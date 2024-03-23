bb1f1715f8b57fb993afd597053cd14f1f040767
Quinos2003 <asthana.samarth2@gmail.com>
2024-03-23 23:38:15 +0530

    Basic template for cf contests

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void f(int n ,int k, vector<int>& arr){
+    int res =0;
+
+    cout << res << endl;
+}
+int main() {
+    int ttt;
+    cin >> ttt;
+    while (ttt--) {
+        
+        int n, k;
+        cin >> n >> k;
+        // int n; string s; cin >> n; cin>>s;
+        vector<int> a(n); for (int i = 0; i < n; ++i) cin >> a[i];
+        // vector<int> b(m); for (int i = 0; i < m; ++i) cin >> b[i];
+        
+        f(n,k,a);
+    }
+
+    return 0;
+}
